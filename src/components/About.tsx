7a7f2ad4b02b91cace6acecd325de452aa368f5f
Kirill Ginko <kirillginko@gmail.com>
2022-11-16 13:00:59 -0500

    added description to about section fixed css

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -22,7 +22,10 @@ export default function About() {
         <h1 className={styles.location_title}>Based In NYC</h1>
         </div>
         <div className={styles.description_container} data-scroll data-scroll-section>
-          <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit tenetur molestiae maiores ipsum est ea doloremque repellendus! Vitae, minus quaerat!</h1>
+          <div className={styles.description_inner}>
+          <div className={styles.description_left}><h2 className={styles.title}>A journey of curiosity and exploration</h2></div>
+          <div className={styles.description_right}><h2 className={styles.title}>I'm a creative designer and developer with experience in building products and innovative digital experiences. I've helped brands build innovative experiences and create accessible user-friendly experiences.</h2></div>
+          </div>
         </div>
         </>
         
